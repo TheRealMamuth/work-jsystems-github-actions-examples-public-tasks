2b9dbdea5069c46694501004951ad54edb7c90d0
Piotr Koska <piotr.s.koska@gmail.com>
2026-03-20 13:56:02 +0100

    Update

--- TERRAFORM/03/main.tf
+++ TERRAFORM/03/main.tf
@@ -26,7 +26,7 @@ resource "digitalocean_project_resources" "main" {
 resource "digitalocean_vpc" "main" {
   name     = "piotr-koska-github-actions-vpc"
   region   = "fra1"
-  ip_range = "10.10.10.0/24"
+  ip_range = "10.113.10.0/24" # <- uzjcie sowej sieci
 }
 
 resource "tls_private_key" "main" {
