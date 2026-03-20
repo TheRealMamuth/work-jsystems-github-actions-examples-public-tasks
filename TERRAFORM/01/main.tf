43f9d6e787f66f10d7737ea1fddf22ac8e074138
Piotr Koska <piotr.s.koska@gmail.com>
2026-03-20 13:07:53 +0100

    Update

--- TERRAFORM/01/main.tf
+++ TERRAFORM/01/main.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_droplet" "main" {
-  name   = "piotr-koska-github-actions-droplet"
+  name   = "piotr-koska-github-actions-droplet" # dajcie sowje imie
   region = "fra1"
   size   = "s-1vcpu-1gb"
   image  = "ubuntu-24-04-x64"
